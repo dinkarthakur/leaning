0d7d77e43eb53829ca50d0ef8d9ebe618502f764
dthakur <dthakur@cvent.com>
2024-12-18 07:44:33 +0530

    first commit

---
+++ src/test/java/com/leaning/parent/ParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaning.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
